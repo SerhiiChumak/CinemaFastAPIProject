dd67db8d7268ab9ef7dce36f7a7e2ede793064cf
Serhii Chumak <s4umak@gmail.com>
2026-02-13 14:46:38 +0200

    modified main file add api

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from routes import (
+    movie_router,
+    accounts_router,
+    profiles_router
+)
 
+app = FastAPI(
+    title="Movies Theatre",
+    description="An online cinema is a digital platform that allows users to select, watch, and purchase access to movies and other video materials via the internet."
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+api_version_prefix = "/api/v1"
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
+app.include_router(profiles_router, prefix=f"{api_version_prefix}/profiles", tags=["profiles"])
+app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
